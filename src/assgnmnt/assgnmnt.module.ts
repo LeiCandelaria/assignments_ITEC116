4c74bef8eb2f8ffd074af1310b34ac42ae5ffdd0
Leila Candelaria <leilaanne.candelaria@cvsu.edu.ph>
2024-11-09 04:39:06 +0800

    Update assgnmnt.module.ts

--- src/assgnmnt/assgnmnt.module.ts
+++ src/assgnmnt/assgnmnt.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AssgnmntService,} from './assgnmnt.service';
 import { assgnmntController} from './assgnmnt.controller';
-
+// always import the controller and service in here//
 
 @Module({
   controllers: [assgnmntController],
   providers: [AssgnmntService],
 })
-export class assgnmntModule {
+export class ssgnmntModule {
 
 }
