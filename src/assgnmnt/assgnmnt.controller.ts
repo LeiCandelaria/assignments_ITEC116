b50d12208292994643d7fd8a6083a25422c7de8a
Leila Candelaria <leilaanne.candelaria@cvsu.edu.ph>
2024-11-09 04:28:53 +0800

    Update assgnmnt.controller.ts

--- src/assgnmnt/assgnmnt.controller.ts
+++ src/assgnmnt/assgnmnt.controller.ts
@@ -5,13 +5,14 @@ import { get } from 'http';
 export class assgnmntController {
 
   @Get('factorial/:number')
-  calculateFactorial(@Param('number', ParseIntPipe) number: number): { factorial: number } { //Calculates the factorial and returns it as JSON
+  calculateFactorial(@Param('number', ParseIntPipe) number: number): { factorial: number } { //Calculates the factorial and returns it as JSON//
     const factorial = this.factorial(number);
-    return { factorial };
+    return { factorial }; // Calculates the factorial of the set number//
   }
 
+  //Checks the number if it is factorable//
   public factorial(num: number): number {
-    if (num < 0) throw new Error("Negative numbers cannot be used in Factorial");
+    if (num < 0) throw new Error("Negative integers cannot be used in Factorial"); // Factorial of negative numbers is not defined//
     if (num === 0 || num === 1) return 1;
     let factorial_result = 1;
     for (let i = 2; i <= num; i++) {
