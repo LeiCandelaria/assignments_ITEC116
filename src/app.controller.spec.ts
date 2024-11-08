9da98b747aabf1501512583712e39e81e1753f3a
Leila Candelaria <leilaanne.candelaria@cvsu.edu.ph>
2024-11-09 04:39:52 +0800

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Sir Vince!"', () => {
+      expect(appController.getHello()).toBe('Hello Sir Vince!');
     });
   });
 });
